76baad41baf0fe76910f2bb04c8a6690d569753b
salah1337 <salah1337@protonmail.com>
2020-05-28 19:20:24 +0000

    Created models migrations and controllers (empty)

---
+++ app/Http/Controllers/OptionGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OptionGroup;
+use Illuminate\Http\Request;
+
+class OptionGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OptionGroup  $optionGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OptionGroup $optionGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OptionGroup  $optionGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OptionGroup $optionGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OptionGroup  $optionGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OptionGroup $optionGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OptionGroup  $optionGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OptionGroup $optionGroup)
+    {
+        //
+    }
+}
