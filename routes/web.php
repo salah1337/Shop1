f8c6c0a865e2d982bbb159c8408a47f821a963ed
salah1337 <salah1337@protonmail.com>
2020-08-08 01:48:14 +0100

    Deployment fix.

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::prefix('/option')->group( function(){
 //     Route::get('/', 'OptionController@index');
